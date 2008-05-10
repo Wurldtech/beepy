a670734d27f37a847a0046a7d700b0ad30016233
Justin Warren <justin@eigenmagic.com>
2008-05-10 03:09:24 +0000

    Updated setup for new version Updated INSTALL to reflect new testing mechanism

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-# $Id: setup.py,v 1.9 2007/09/03 03:20:03 jpwarren Exp $
-# $Revision: 1.9 $
+# $Id: setup.py,v 1.10 2008/05/10 03:09:25 jpwarren Exp $
+# $Revision: 1.10 $
 #
 # BEEPy - A Python BEEP Library
-# Copyright (c) 2002-2007 Justin Warren <daedalus@eigenmagic.com>
+# Copyright (c) Justin Warren <daedalus@eigenmagic.com>
 
 from distutils.core import setup
 
 setup(name='BEEPy',
-	version='0.6.1',
+	version='0.6.2',
 	description='A Python BEEP Library',
 	author='Justin Warren',
 	author_email='daedalus@eigenmagic.com',
